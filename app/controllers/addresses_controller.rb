50de1056eb129cdf7e6492e479f8f52da17f268d
Andrew <awood50@uncc.edu>
2018-11-15 16:08:35 +0000

    Complete Create and Show

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,20 @@
 class AddressesController < ApplicationController
+    def show
+        @address = Address.find(params[:id])
+    end    
+    
     def new
     end
     
     def create
-        render plain: params[:address].inspect
+        @address = Address.new(address_params)
+        
+        @address.save
+        redirect_to @address
     end
 end
+
+private
+    def address_params
+        params.require(:address).permit(:name, :Phone_Number, :email, :address)
+    end    
