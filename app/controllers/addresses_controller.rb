9aba723dbeaf0bc6d9efff6e2948a354011f50bc
brownshugga95 <tchacko@uncc.edu>
2018-11-19 22:44:48 +0000

    Create CRUD but not fix index

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,40 @@
 class AddressesController < ApplicationController
+    def index
+        @address = Address.all
+    end
     def show
         @address = Address.find(params[:id])
     end    
     
     def new
+        @address = Address.new
     end
     
     def create
         @address = Address.new(address_params)
         
-        @address.save
-        redirect_to @address
+        if @address.save
+            redirect_to @address
+        else
+            render 'new'
+        end
+    end
+    def edit
+        @address = Address.find(params[:id])
+    end
+    def update
+        @address = Address.find(params[:id])
+        if @address.update(article_params)
+            redirect_to @address
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @address = Address.find(params[:id])
+        @address.destroy
+        
+        redirect_to articles_path
     end
 end
 
