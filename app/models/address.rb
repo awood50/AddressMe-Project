ea24381ab6b478d365281fe97526363e097c8a51
Cam <cmcinto5@uncc.edu>
2018-11-19 23:39:39 +0000

    Add validations for new address entry

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,6 @@
 class Address < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }
+    validates :Phone_Number, presence: true, length: { minimum: 10 }
+    validates :email, presence: true, length: { minimum: 6 }
+    validates :address, presence: true, length: { minimum: 6 }
 end
