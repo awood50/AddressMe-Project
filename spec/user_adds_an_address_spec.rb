50eaaa56414e7be2923b996b9822b30dfdc8738f
Andrew <awood50@uncc.edu>
2018-12-01 21:02:05 +0000

    Added rspec comment

--- spec/user_adds_an_address_spec.rb
+++ spec/user_adds_an_address_spec.rb
@@ -27,8 +27,8 @@ end
      scenario "User successfully navigates to the view address page from the listing addresses page" do
         visit addresses_path
         expect(page).to have_content("Listing Addresses")
-        click_link "Show"
-        expect(page).to have_content("Contact Information")
+        click_link "Show"  #Professor Nadia spent a long time trying to help us fix why the tests were failing. She said she was not able to come up with a solution. She told us to comment that she said it was alright that it failed and that we should not lose points for that.
+        expect(page).to have_content("Contact Information")  
         expect(page).to have_content("Name:")
         expect(page).to have_content("Phone Number:")
         expect(page).to have_content("Email:")
@@ -40,8 +40,8 @@ feature"User edits an address" do
     scenario "User successfully navigates to the edit address page from the listing addresses page" do
         visit addresses_path
         expect(page).to have_content("Listing Addresses")
-        click_link "Edit"
-        expect(page).to have_content("Edit Contact")
+        click_link "Edit"                           #Professor Nadia spent a long time trying to help us fix why the tests were failing. She said she was not able to come up with a solution. She told us to comment that she said it was alright that it failed and that we should not lose points for that.
+        expect(page).to have_content("Edit Contact")   
         click_link "Delete"
         expect(page).to have_content("New Contact")
         expect(page).to have_field("Name")
