5cd2cdb0e6e3c3604905cd314e1cd4eb81c6624a
Andrew <awood50@uncc.edu>
2018-12-01 20:50:56 +0000

    Finished Rspec

--- spec/user_adds_an_address_spec.rb
+++ spec/user_adds_an_address_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper.rb'
 
+feature"User enters website" do
+    scenario "User successfully navigates to the listing addreses page from the welcome page" do
+        visit welcome_index_path
+        expect(page).to have_content("AddressMe")
+        click_link "AddressMe"
+        expect(page).to have_content("Listing Addresses")
+    end
+end
+
 feature"User adds an address" do
     scenario "User successfully navigates to the new address page from the listing addresses page" do
         visit addresses_path
@@ -19,10 +28,24 @@ end
         visit addresses_path
         expect(page).to have_content("Listing Addresses")
         click_link "Show"
-        #expect(page).to have_content("New Contact")
+        expect(page).to have_content("Contact Information")
+        expect(page).to have_content("Name:")
+        expect(page).to have_content("Phone Number:")
+        expect(page).to have_content("Email:")
+        expect(page).to have_content("Address:")
+    end    
+end
+
+feature"User edits an address" do
+    scenario "User successfully navigates to the edit address page from the listing addresses page" do
+        visit addresses_path
+        expect(page).to have_content("Listing Addresses")
+        click_link "Edit"
+        expect(page).to have_content("Edit Contact")
         expect(page).to have_field("Name")
         expect(page).to have_field("Phone number")
         expect(page).to have_field("Email")
         expect(page).to have_field("Address")
-    end    
-end
+ 
+    end
+end    
